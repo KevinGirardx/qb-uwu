129c6eac966a6775afbecfd1df027d40999389c4
KevinGirardx <89563654+KevinGirardx@users.noreply.github.com>
2022-01-17 21:07:55 -0400

    Update main.lua

--- qb-uwu/server/main.lua
+++ qb-uwu/server/main.lua
@@ -1,30 +1,20 @@
 QBCore = exports["qb-core"]:GetCoreObject()
 
---only event used for UwU, unless ingredients otherwise added 
 RegisterServerEvent("qb-uwu:bill:player")
 AddEventHandler("qb-uwu:bill:player", function(playerId, amount)
         local biller = QBCore.Functions.GetPlayer(source)
         local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
         local amount = tonumber(amount)
-        if biller.PlayerData.job.name == 'uwu' then
+        if biller.PlayerData.job.name == 'burgershot' then
             if billed ~= nil then
                 if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
-                       if Config.SQL == 'oxmysql' then
-                        exports.oxmysql:insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
+                        MySQL.Async.insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
                             ['citizenid'] = billed.PlayerData.citizenid,
                             ['amount'] = amount,
                             ['society'] = biller.PlayerData.job.name,
                             ['sender'] = biller.PlayerData.charinfo.firstname
                         })
-                        else
-                          exports.ghmattimysql:execute('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (@citizenid, @amount, @society, @sender)', {
-                            ['@citizenid'] = billed.PlayerData.citizenid,
-                            ['@amount'] = amount,
-                            ['@society'] = biller.PlayerData.job.name,
-                            ['@sender'] = biller.PlayerData.charinfo.firstname
-                           })
-                       end
                         TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
                         TriggerClientEvent('QBCore:Notify', source, 'Invoice Successfully Sent', 'success')
                         TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
@@ -175,3 +165,4 @@ QBCore.Functions.CreateCallback('qb-uwu:server:get:ingredientcSandwich', functio
         cb(false)
     end
 end)
+
